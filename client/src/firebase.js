ebacfeda1a9e4294ed0579484f51f1a45145f049
Yashraj <yashsharmau9@gmail.com>
2024-02-28 19:44:30 +0530

    Added Files

---
+++ client/src/firebase.js
@@ -0,0 +1,40 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "mern-estate.firebaseapp.com",
+//   projectId: "mern-estate",
+//   storageBucket: "mern-estate.appspot.com",
+//   messagingSenderId: "1078482850952",
+//   appId: "1:1078482850952:web:28f19139ab77246602fb3d",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB-yVjCg6cE8zI79ljqpmlFupf-KPSSD7w",
+  authDomain: "mern-state-ae06b.firebaseapp.com",
+  projectId: "mern-state-ae06b",
+  storageBucket: "mern-state-ae06b.appspot.com",
+  // gs://mern-state-ae06b.appspot.com
+  messagingSenderId: "298703540784",
+  appId: "1:298703540784:web:4db2b73f0c0681d9b353f5",
+  measurementId: "G-SLFFST6ZGD",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+//jwt - 2:25
